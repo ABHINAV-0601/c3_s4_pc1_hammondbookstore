6d9cf29cfa77dda21588f5853de54507edb7e540
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2022-08-18 14:22:53 +0530

    declared getter setter for book class

--- src/Book.java
+++ src/Book.java
@@ -4,4 +4,28 @@ public class Book {
     private String description;
     private double price;
 
+    public long getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(long isbn) {
+        this.isbn = isbn;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
